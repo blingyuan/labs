3dfd7d7d7124ee2f4d1a44cf573939ab50ac8615
yuan.bie <yuan.bie@ucarinc.com>
2021-01-11 14:49:39 +0800

    HttpMessageConverter 消息转换器

--- lab-for-spring-boot-mvc/src/main/java/com/smallyuan/labs/springmvc/controller/UserController.java
+++ lab-for-spring-boot-mvc/src/main/java/com/smallyuan/labs/springmvc/controller/UserController.java
@@ -6,6 +6,7 @@ import com.smallyuan.labs.springmvc.vo.UserVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -59,7 +60,41 @@ public class UserController {
         throw new ServiceException(ServiceExceptionEnum.USER_NOT_FOUND);
     }
 
-    // http://www.iocoder.cn/Spring-Boot/SpringMVC/ todo
+
+    /**
+     * {@link HttpMessageConverter}
+     *
+     * 是否可以读取指定的mediaType内容类型，转换为对应的 clazz 对象
+     *  boolean canRead(Class<?> clazz, @Nullable MediaType mediaType);
+     *
+     * 是否可以将clazz对象，序列化为mediaType内容类型
+     *  boolean canWrite(Class<?> clazz, @Nullable MediaType mediaType);
+     *
+     * 获取HttpMessageConverter能够支持的内容类型
+     *  List<MediaType> getSupportedMediaTypes();
+     *
+     * 读取请求内容，转换成clazz对象
+     *  T read(Class<? extends T> clazz, HttpInputMessage inputMessage)
+     * 			throws IOException, HttpMessageNotReadableException;
+     *
+     * 将clazz对象，序列化为 contentType内容类型，写到响应。
+     * 	void write(T t, @Nullable MediaType contentType, HttpOutputMessage outputMessage)
+     * 			throws IOException, HttpMessageNotWritableException;
+     *
+     * 在请求时，我们在请求头 Content-Type 上，表示请求内容（RequestBody）的内容类型，SpringMvc会从自己的HttpMessageConverter数组中，
+     * 通过canRead 方法，判断能否读取mediaType内容类型，转换为clazz对象。可以的化，调用read方法读取请求内容，转换为clazz对象
+     *
+     * 在响应时，我们在请求头 Accept 上，表示请求内容（ResponseBody）的内容类型，SpringMvc会从自己的HttpMessageConverter数组中，
+     * 通过 canWrite 方法，判断是否能够将clazz对象，序列化为mediaType内容类型，可以的话，调用 write 方法，将clazz 序列化为 contentType
+     * 内容类型，写入到响应
+     * @param userVo
+     * @return
+     */
+    /**
+     * 同时支持 JSON/XML 格式提交数据，也同时支持 JSON/XML 格式响应数据。
+     * @param userVo
+     * @return
+     */
     @PostMapping(value = "/add",
             // ↓ 增加 "application/xml"、"application/json" ，针对 Content-Type 请求头
             consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
